de751f481ea85e1f5b9d26ee225468523096ddd5
Andy Duong <andydduong12@gmail.com>
2023-10-29 22:33:41 -0700

    added basic table

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,7 +1,9 @@
+from flask import redirect, url_for, render_template
+
 from flaskr import app
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
