cb06ccb889d673b56784a4ea3a71bd6a8ca8bae5
Andy Duong <andydduong12@gmail.com>
2023-12-05 22:38:26 -0800

    updated with db classes, and changed some frontend

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_pfp = db.Column(db.String(16), nullable=False, default='default_pfp.jpeg')
+    image_pfp = db.Column(db.String(16), nullable=False, default='images/default_pfp.jpeg')
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
@@ -24,14 +24,12 @@ class User(db.Model):
 class CPU_db(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), unique=True, nullable=False)
-    brand = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float(precision=2))
     clockSpeed = db.Column(db.Float(precision=1), nullable=False)
 
 class CPUCooler_db(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), unique=True, nullable=False)
-    brand = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float(precision=2))
     size = db.Column(db.Integer)
     rpm = db.Column(db.String(20))
@@ -39,7 +37,6 @@ class CPUCooler_db(db.Model):
 class Mobo_db(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), unique=True, nullable=False)
-    brand = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float(precision=2))
     mem_slot = db.Column(db.Integer, nullable=False)
     mem_speed = db.Column(db.Integer, nullable=False)
@@ -49,7 +46,6 @@ class Mobo_db(db.Model):
 class GPU_db(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), unique=True, nullable=False)
-    brand = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float(precision=2))
     #pcie_gen = db.Column(db.String(10), nullable=False)
     mem = db.Column(db.Integer, nullable=False)
@@ -60,14 +56,39 @@ class GPU_db(db.Model):
 class RAM_db(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     model = db.Column(db.String(50), unique=True, nullable=False)
-    brand = db.Column(db.String(10), nullable=False)
     price = db.Column(db.Float(precision=2))
     speed = db.Column(db.String(20)) #ddr4-3600
     modules = db.Column(db.String(20)) # 2 x 8, 16
     cas_latency = db.Column(db.Integer)
 
+class drive_db(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(50), unique=True, nullable=False)
+    price = db.Column(db.Float(precision=2))
+    capacity = db.Column(db.Integer)
+    drive_type = db.Column(db.String(10))
+    interface = db.Column(db.String(20)) #M.2 PCIe 4.0x4
 
+class PSU_db(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(50), unique=True, nullable=False)
+    price = db.Column(db.Float(precision=2))
+    form_factor = db.Column(db.String(5))
+    effi = db.Column(db.String(10))
+    wattage = db.Column(db.Integer)
+    mod = db.Column(db.String(10))
 
+class case_db(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(50), unique=True, nullable=False)
+    price = db.Column(db.Float(precision=2))
+    type = db.Column(db.String(20))
+    color = db.Column(db.String(20))
+    side_panel = db.Column(db.String(25))
 
-
-
+class fans_db(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(50), unique=True, nullable=False)
+    price = db.Column(db.Float(precision=2))
+    size = db.Column(db.Integer)
+    rpm = db.Column(db.String(20))
